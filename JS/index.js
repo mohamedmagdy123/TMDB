47f72989e9b3a1df74fb0374ec5018c44abf6197
MohamedMagdy <mohamedmagdy7770@gmail.com>
2023-06-09 03:12:13 +0300

    pagination
    added paginating to see more movies

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,23 @@
 let data = [];
-let cartoona = "";
+let pages = document.getElementsByClassName("page-link");
+let moviePage = 1;
 
+async function getMovies(page) {
+  let response = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=2e6a6449322614c48c47daede1e94909&include_adult=false&include_video=false&language=en-US&page=${page}`
+  );
+  response = await response.json();
+  data = response.results;
+  console.log(data);
+  displayMovies();
+}
 function displayMovies() {
+  let cartoona = "";
   let movieList = document.getElementById("movieList");
   for (let i = 0; i < data.length; i++) {
     cartoona += `
-        <div class="col-lg-3 col-sm-6 my-3">
-        <div class="card h-100 shadow border-0" style="width: 18rem;">
+        <div class="col-lg-3 col-sm-4 my-3">
+        <div class="card h-100 shadow border-0">
           <img class="card-img-top" src="https://image.tmdb.org/t/p/w500${
             data[i].poster_path
           }
@@ -27,11 +38,10 @@ function displayMovies() {
   movieList.innerHTML = cartoona;
 }
 
-(async function () {
-  let response = await fetch(
-    "https://api.themoviedb.org/3/trending/movie/week?api_key=2e6a6449322614c48c47daede1e94909"
-  );
-  response = await response.json();
-  data = response.results;
-  displayMovies();
-})();
+for (let i = 0; i < pages.length; i++) {
+  pages[i].addEventListener("click", function () {
+    moviePage = pages[i].innerHTML;
+    getMovies(moviePage);
+  });
+}
+getMovies(moviePage);
